d974aa84306a71315cd00183c403c93a8a34882a
tejashwar27 <tejashwar27@gmail.com>
2023-09-12 16:52:01 +0530

    Made bar plot script generic

--- matplotlib/bar_plot.py
+++ matplotlib/bar_plot.py
@@ -3,6 +3,14 @@
 # Required packages can be installed using following command
 # pip install pandas
 
+# How to run??
+# python3 <path_to_csv> <no_of_bars> <first_data> <output_pdf>
+
+# Assumptions:
+# X-axis is first column in the CSV file.
+# All of the bars are columns one after other
+
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -11,8 +19,12 @@ import sys
 
 csv_file = str(sys.argv[1])
 
-c = 4
+c = int(sys.argv[2])
 w = 0.8/c
+
+first = int(sys.argv[3])
+
+outpdf = str(sys.argv[4])
 
 # Reading CSV file
 data = pd.read_csv(csv_file)
@@ -21,39 +33,29 @@ df = pd.DataFrame(data)
 
 print(df)
 
-X = list(df.iloc[2:27, 0])
-Y1 = [float(y) for y in list(df.iloc[2:27, 7])]
-Y2 = [float(y) for y in list(df.iloc[2:27, 8])]
-Y3 = [float(y) for y in list(df.iloc[2:27, 9])]
-Y4 = [float(y) for y in list(df.iloc[2:27, 10])]
+X = list(df.iloc[1:27, 0])
 
-l1 = df.iloc[0, 7]
-l2 = df.iloc[0, 8]
-l3 = df.iloc[0, 9]
-l4 = df.iloc[0, 10]
+print(X)
 
-print(l1)
-print(l2)
-print(l3)
-print(l4)
+Y = []
+L = []
 
+for i in range(c):
+    print(i)
+    Y.append([float(y) for y in list(df.iloc[1:27, first+i])])
+    L.append(df.iloc[0, first+i])
 
 x_axis = np.arange(len(X))
 
 print(x_axis)
-
-# print(X)
-# print(Y)
 
 plt.rcParams.update({'font.size': 15})
 
 fig = plt.figure(figsize=(16, 9))
 
 # Plot the data using bar() method
-plt.bar(x_axis + 0*w, Y1, w, label=l1, edgecolor='black')
-plt.bar(x_axis + 1*w, Y2, w, label=l2, edgecolor='black')
-plt.bar(x_axis + 2*w, Y3, w, label=l3, edgecolor='black')
-plt.bar(x_axis + 3*w, Y4, w, label=l4, edgecolor='black')
+for k in range(c):
+    plt.bar(x_axis + k*w, Y[k], w, label=L[k], edgecolor='black')
 
 t = w*(c-1)/2
 
@@ -73,6 +75,6 @@ plt.legend(ncol=c)
 plt.grid(which='major', axis='y', color='#666666', linestyle='-')
 plt.grid(which='minor', axis='y', color='#999999', linestyle='-', alpha=0.5)
 
-plt.savefig("f1.pdf", edgecolor="black", bbox_inches="tight")
+plt.savefig(outpdf, edgecolor="black", bbox_inches="tight")
 # Show the plot
 plt.show()
